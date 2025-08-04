f66bf5057598108242355472af151ef3e90793a2
Maga01A <alnatluxanov@gmail.com>
2025-08-04 10:10:45 +0000

    Add 6-size.c printing sizes of various types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
-
-/**
- * main - prints sizes of various types
- *
- * Return: Always 0
- */
 int main(void)
 {
     printf("Size of a char: %zu byte(s)\n", sizeof(char));
@@ -14,3 +8,4 @@ int main(void)
     printf("Size of a float: %zu byte(s)\n", sizeof(float));
     return (0);
 }
+
