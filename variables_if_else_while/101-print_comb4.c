9fa804d9980edfd9cc156bdbc364aaaefc686b83
Maga01A <alnatluxanov@gmail.com>
2025-08-04 12:26:23 +0000

    101

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of three distinct digits
+ * main - Prints all possible combinations of two two-digit numbers
+ *
+ * Description: Prints number pairs from 00 01 to 98 99 in ascending order
+ *              with proper formatting. Each number is printed as two digits
+ *              (e.g., 1 becomes 01). Pairs are separated by comma and space.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, j, k;
+    int i, j, k;
 
-	for (i = 0; i < 8; i++)
-	{
-		for (j = i + 1; j < 9; j++)
-		{
-			for (k = j + 1; k < 10; k++)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(k + '0');
+    for (i = 0; i < 8; i++)
+    {
+        for (j = i + 1; j < 9; j++)
+        {
+            for (k = j + 1; k < 10; k++)
+            {
+                putchar(i + '0');
+                putchar(j + '0');
+                putchar(k + '0');
+                
+                if (i != 7 || j != 8 || k != 9)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    putchar('\n');
 
-				if (i != 7 || j != 8 || k != 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-
-	return (0);
-}}
+    return (0);
+}
