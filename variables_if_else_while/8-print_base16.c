56920b3519ff215e902a93f834fd74c41670d052
Maga01A <alnatluxanov@gmail.com>
2025-08-04 11:42:49 +0000

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers of base 16 in lowercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char c;
+
+    /* print '0' through '9' */
+    for (c = '0'; c <= '9'; c++)
+        putchar(c);
+
+    /* print 'a' through 'f' */
+    for (c = 'a'; c <= 'f'; c++)
+        putchar(c);
+
+    /* trailing newline */
+    putchar('\n');
+
+    return (0);
+}
+
