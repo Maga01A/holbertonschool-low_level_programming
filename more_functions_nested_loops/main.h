1f85db1d40ab825ad0519c14ae98180fcc77b345
Maga01A <alnatluxanov@gmail.com>
2025-08-05 12:50:19 +0000

    Add print_diagonal function and corresponding test/main files

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,17 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _isupper(int c);
-int _isdigit(int c);
+int _putchar(char c);
+void print_diagonal(int n);
 
 #endif /* MAIN_H */
-int _isdigit(int c);
-void more_numbers(void);
-int _putchar(char c);
-void more_numbers(void);
-void print_line(int n);
-int _putchar(char c);
-void print_line(int n);
-void print_line(int n);
-int _putchar(char c);
-void print_line(int n);
