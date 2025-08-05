5a9a31ff1f4d216e42d8d23a11cfdc4b734ca800
Maga01A <alnatluxanov@gmail.com>
2025-08-05 14:12:15 +0000

    6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,20 @@
 #include "main.h"
-
 /**
  * print_line - draws a straight line of length n
  * @n: number of times to print '_'
  */
 void print_line(int n)
 {
-    int i;
+	int i;
 
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	
+	for (i = 0; i < n; i++)
+		_putchar('_');
 
-    for (i = 0; i < n; i++)
-        _putchar('_');
-
-    _putchar('\n');
+	_putchar('\n');
 }
