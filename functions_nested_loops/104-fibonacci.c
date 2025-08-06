783cd95266b0a141ba1bf476cd0745a51f23c53b
Maga01A <alnatluxanov@gmail.com>
2025-08-06 10:37:48 +0000

    104

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -2,6 +2,11 @@
 
 #define BASE 1000000000UL  /* 10^9, 9 basamak */
 
+/**
+ * print_number - prints a number split into high and low parts
+ * @high: the high part of the number
+ * @low: the low part of the number
+ */
 void print_number(unsigned long high, unsigned long low)
 {
 	if (high)
@@ -10,6 +15,11 @@ void print_number(unsigned long high, unsigned long low)
 		printf("%lu", low);
 }
 
+/**
+ * main - prints the first 98 Fibonacci numbers starting with 1 and 2
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	unsigned long fib1_high = 0, fib1_low = 1;
