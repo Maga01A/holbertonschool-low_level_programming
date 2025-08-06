ff09df495e67ea707f7167b519b87f0c12e8d32c
Maga01A <alnatluxanov@gmail.com>
2025-08-06 10:34:47 +0000

    103

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - computes and prints the sum of even Fibonacci terms <= 4,000,000
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long fib1 = 1, fib2 = 2, next;
+	unsigned long sum = 2; /* fib2 is even and <= 4,000,000 */
+	
+	while (1)
+	{
+		next = fib1 + fib2;
+		if (next > 4000000)
+			break;
+		if ((next % 2) == 0)
+			sum += next;
+		fib1 = fib2;
+		fib2 = next;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
