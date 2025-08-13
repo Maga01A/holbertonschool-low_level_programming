6b9e5350f0778eca70250cf6f196fd91905cea42
Maga01A <alnatluxanov@gmail.com>
2025-08-13 10:12:05 +0000

    Implement _atoi function to convert string to integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <limits.h>
-
 /**
  * _atoi - convert a string to an integer
  * @s: the string to convert
@@ -9,32 +8,30 @@
  */
 int _atoi(char *s)
 {
-    int i = 0, sign = 1, num = 0, started = 0;
+	int i = 0, sign = 1, num = 0, started = 0;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        else if (s[i] == '+')
-        {
-            /* ignore plus sign */
-        }
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            started = 1;
-
-            /* Overflow check */
-            if (sign == 1 && num > (INT_MAX - (s[i] - '0')) / 10)
-                return INT_MAX;
-            if (sign == -1 && num > (INT_MAX - (s[i] - '0')) / 10)
-                return INT_MIN;
-
-            num = num * 10 + (s[i] - '0');
-        }
-        else if (started)
-            break;
-        i++;
-    }
-    return (num * sign);
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] == '+')
+		{
+			/* ignore plus sign */
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			started = 1;
+			
+			/* Overflow check */
+			if (sign == 1 && num > (INT_MAX - (s[i] - '0')) / 10)
+				return INT_MAX;
+			if (sign == -1 && num > (INT_MAX - (s[i] - '0')) / 10)
+				return INT_MIN;
+			num = num * 10 + (s[i] - '0');
+		}
+		else if (started)
+			break;
+		i++;
+	}
+	return (num * sign);
 }
-
