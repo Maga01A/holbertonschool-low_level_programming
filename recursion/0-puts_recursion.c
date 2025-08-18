cee78eca50e06ab367847de98f7b96224ba62dca
Maga01A <alnatluxanov@gmail.com>
2025-08-18 10:30:44 +0000

    Fix trailing whitespace and Betty style for _puts_recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,18 +1,16 @@
-#include "main.h"
-
-/**
- * _puts_recursion - prints a string followed by a new line
- * @s: string to print
+#include "main.h"/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to string
+ *
+ * Return: Nothing
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') /* stringin sonuna gelince fonksiyonu bitir */
+	if (*s == '\0')
 	{
-		_putchar('
-');
+		_putchar('\n');
 		return;
 	}
-
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
