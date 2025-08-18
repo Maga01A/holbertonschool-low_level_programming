40993c050858cea7bdf4e11a9947070b588c59a5
Maga01A <alnatluxanov@gmail.com>
2025-08-18 10:31:48 +0000

    Fix Betty-doc for _puts_recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string followed by a new line
+ * _puts_recursion - prints a string, followed by a new line
  * @s: pointer to the string to print
  *
  * Description: Recursively prints each character of the string
  *              using _putchar, then prints a newline.
+ *
+ * Return: Nothing
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') /* stringin sonuna gelince fonksiyonu bitir */
+	if (*s == '\0')
 	{
 		_putchar('
 ');
