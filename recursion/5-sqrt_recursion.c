5f3991ef6a7f6fa2e0d7ffc3e9b5862a7d04bd4e
Maga01A <alnatluxanov@gmail.com>
2025-08-18 10:17:11 +0000

    Fix Betty style for _sqrt_recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,16 +2,16 @@
 
 int sqrt_helper(int n, int i)
 {
-    if (i * i == n)
-        return i;
-    if (i * i > n)
-        return -1;
-    return sqrt_helper(n, i + 1);
+	if ((i * i) == n)
+		return (i);
+	if ((i * i) > n)
+		return (-1);
+	return (sqrt_helper(n, i + 1));
 }
 
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return -1;
-    return sqrt_helper(n, 1);
+	if (n < 0)
+		return (-1);
+	return (sqrt_helper(n, 1));
 }
