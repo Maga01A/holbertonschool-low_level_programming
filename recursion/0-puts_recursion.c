32f79a9da6200ad9e2e7eb99b5419a54eff26794
Maga01A <alnatluxanov@gmail.com>
2025-08-18 10:31:16 +0000

    Add Betty-doc description for _puts_recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,16 +1,21 @@
-#include "main.h"/**
- * _puts_recursion - prints a string, followed by a new line
- * @s: pointer to string
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: pointer to the string to print
  *
- * Return: Nothing
+ * Description: Recursively prints each character of the string
+ *              using _putchar, then prints a newline.
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s == '\0') /* stringin sonuna gelince fonksiyonu bitir */
 	{
-		_putchar('\n');
+		_putchar('
+');
 		return;
 	}
+
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
