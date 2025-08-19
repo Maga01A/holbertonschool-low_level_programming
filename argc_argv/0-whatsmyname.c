9af5906b85efc8719ea96f11edaf920aefb9734a
Maga01A <alnatluxanov@gmail.com>
2025-08-19 11:51:44 +0000

    0-whatsmyname: print program name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - prints the name of the program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
-    (void)argc;  /* argc istifadə olunmur, warning verməmək üçün */
-    printf("%s\n", argv[0]);  /* argv[0] proqramın adını saxlayır və çap edirik */
-    return 0;  /* proqram uğurla başa çatdı */
+	(void)argc; /* argc istifadə olunmur, warning verməmək üçün */
+	printf("%s\n", argv[0]); /* argv[0] proqramın adını saxlayır və çap edirik */
+	return (0); /* proqram uğurla başa çatdı */
 }
 
