2f45e77997da5d7d01b0b13f9e62400e24f135eb
Maga01A <alnatluxanov.com>
2025-08-20 15:34:01 +0000

    0-create_array.c: implement create_array function using malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>  /* malloc ve NULL için gerekli */
+#include <stdlib.h> /* malloc ve NULL için gerekli */
 
 /**
  * create_array - verilen boyutta bir dizi oluşturur ve
@@ -10,27 +10,22 @@
  */
 char *create_array(unsigned int size, char c)
 {
-    char *array;        /* malloc ile oluşturulan dizinin adresini tutacak pointer */
-    unsigned int i;     /* for döngüsünde sayaç olarak kullanılacak değişken */
+	char *array; /* malloc ile oluşturulan dizinin adresini tutacak pointer */
+	unsigned int i; /* for döngüsünde sayaç olarak kullanılacak değişken */
 
-    /* Eğer size = 0 ise, dizi oluşturmak mantıksız -> NULL döndür */
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    /* malloc ile bellekten 'size' adet char için yer ayırıyoruz */
-    array = malloc(size * sizeof(char));
+	array = malloc(size * sizeof(char));
 
-    /* malloc başarısız olursa (yeterli bellek yoksa) -> NULL döndür */
-    if (array == NULL)
-        return (NULL);
+	if (array == NULL)
+		return (NULL);
 
-    /* Döngü ile tüm diziyi verilen karakter 'c' ile doldur */
-    for (i = 0; i < size; i++)
-    {
-        array[i] = c;  /* her elemanı 'c' yap */
-    }
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
 
-    /* Dizinin başlangıç adresini (pointer) döndür */
-    return (array);
+	return (array);
 }
 
