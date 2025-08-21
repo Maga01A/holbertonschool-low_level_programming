b7c31773075df7688d5d531857e69e04054ee1bf
Maga01A <alnatluxanov.com>
2025-08-21 13:16:08 +0000

    Add _calloc function to allocate and zero-initialize memory

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - array üçün yaddaş ayırır və sıfırlayır
+ * @nmemb: element sayı
+ * @size: hər elementin ölçüsü (byte)
+ *
+ * Return: pointer (malloc ilə ayrılır), NULL əgər uğursuz olsa
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    unsigned int total_size, i;
+    char *ptr;
+
+    if (nmemb == 0 || size == 0)
+        return NULL;
+
+    total_size = nmemb * size;
+
+    ptr = malloc(total_size);
+    if (ptr == NULL)
+        return NULL;
+
+    /* Yaddaşı sıfırlayırıq */
+    for (i = 0; i < total_size; i++)
+        ptr[i] = 0;
+
+    return (ptr);
+}
+
