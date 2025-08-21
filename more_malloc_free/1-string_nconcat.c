24fe92ff8fd4b878b89b747c7ef451be03d0afbd
Maga01A <alnatluxanov.com>
2025-08-21 13:23:09 +0000

    Add string_nconcat function and update main.h with prototype

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -7,35 +7,39 @@
  * @s2: ikinci string
  * @n: s2-dən götürüləcək simvolların sayı
  *
- * Return: yeni string pointeri (malloc ilə ayrılır), NULL əgər uğursuz olsa
+ * Return: yeni string pointeri (malloc ilə ayrılır),
+ *         NULL əgər uğursuz olsa
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, len1 = 0, len2 = 0;
-	char *result;
+    unsigned int i, j, len1 = 0, len2 = 0;
+    char *result;
 
-	if (!s1) 
-		s1 = "";
-	if (!s2) 
-		s2 = "";
+    if (!s1)
+        s1 = "";
+    if (!s2)
+        s2 = "";
 
-	while (s1[len1]) 
-		len1++;
-	while (s2[len2]) 
-		len2++;
-	
-	if (n > len2) 
-		n = len2;
+    while (s1[len1])
+        len1++;
+    while (s2[len2])
+        len2++;
 
-	result = malloc(len1 + n + 1);
-	if (!result) 
-		return NULL;
+    if (n > len2)
+        n = len2;
 
-	for (i = 0; i < len1; i++) 
-		result[i] = s1[i];
-	for (j = 0; j < n; j++) 
-		result[i + j] = s2[j];
-	result[i + j] = '\0';
+    result = malloc(len1 + n + 1);
+    if (!result)
+        return (NULL);
 
-	return result;
+    for (i = 0; i < len1; i++)
+        result[i] = s1[i];
+
+    for (j = 0; j < n; j++)
+        result[i + j] = s2[j];
+
+    result[i + j] = '\0';
+
+    return (result);
 }
+
