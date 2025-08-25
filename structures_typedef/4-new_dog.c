8120382dfad62affc3cb63ca77b151d0f2ecf849
Maga01A <alnatluxanov@gmail.com>
2025-08-25 12:59:01 +0000

    betty fixed

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,35 @@
 #include <stdlib.h>  /* malloc ve free için */
-#include "dog.h"      /* struct dog ve prototipler için */
+#include "dog.h"     /* struct dog ve prototipler için */
+
+/**
+ * str_copy - Bir string'in kopyasını oluşturur
+ * @str: Kopyalanacak string
+ *
+ * Return: Yeni string'in adresi veya NULL
+ */
+char *str_copy(char *str)
+{
+    char *copy;
+    int len = 0, i;
+
+    if (str == NULL)
+        return (NULL);
+
+    /* Uzunluğu hesapla */
+    while (str[len] != '\0')
+        len++;
+
+    /* Bellek ayır */
+    copy = malloc(len + 1);
+    if (copy == NULL)
+        return (NULL);
+
+    /* Karakterleri kopyala */
+    for (i = 0; i <= len; i++)
+        copy[i] = str[i];
+
+    return (copy);
+}
 
 /**
  * new_dog - Yeni bir dog_t oluşturur ve kopyalarını saklar
@@ -7,71 +37,38 @@
  * @age: Köpeğin yaşı
  * @owner: Köpeğin sahibi
  *
- * Return: Başarılı ise yeni dog_t işaretçisi, başarısız ise NULL
+ * Return: Başarılıysa yeni dog_t işaretçisi, başarısızsa NULL
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
-	char *name_copy;
-	char *owner_copy;
-	int i, name_len, owner_len;
+    dog_t *d;
 
-	d = malloc(sizeof(dog_t));
-	if (d == NULL)
-		return (NULL);
+    /* dog_t için bellek ayır */
+    d = malloc(sizeof(dog_t));
+    if (d == NULL)
+        return (NULL);
 
-	/* name uzunluğunu hesapla */
-	name_len = 0;
-	if (name != NULL)
-	{
-		while (name[name_len] != '\0')
-			name_len++;
-	}
+    /* name kopyası oluştur */
+    d->name = str_copy(name);
+    if (name != NULL && d->name == NULL)
+    {
+        free(d);
+        return (NULL);
+    }
 
-	/* owner uzunluğunu hesapla */
-	owner_len = 0;
-	if (owner != NULL)
-	{
-		while (owner[owner_len] != '\0')
-			owner_len++;
-	}
+    /* owner kopyası oluştur */
+    d->owner = str_copy(owner);
+    if (owner != NULL && d->owner == NULL)
+    {
+        if (d->name != NULL)
+            free(d->name);
+        free(d);
+        return (NULL);
+    }
 
-	/* name için bellek ayır ve kopyala */
-	if (name != NULL)
-	{
-		name_copy = malloc(name_len + 1);
-		if (name_copy == NULL)
-		{
-			free(d);
-			return (NULL);
-		}
-		for (i = 0; i <= name_len; i++)
-			name_copy[i] = name[i]; /* '\0' dahil kopyala */
-		d->name = name_copy;
-	}
-	else
-		d->name = NULL;
+    /* age değerini ata */
+    d->age = age;
 
-	/* owner için bellek ayır ve kopyala */
-	if (owner != NULL)
-	{
-		owner_copy = malloc(owner_len + 1);
-		if (owner_copy == NULL)
-		{
-			if (d->name != NULL)
-				free(d->name);
-			free(d);
-			return (NULL);
-		}
-		for (i = 0; i <= owner_len; i++)
-			owner_copy[i] = owner[i]; /* '\0' dahil kopyala */
-		d->owner = owner_copy;
-	}
-	else
-		d->owner = NULL;
-
-	d->age = age;
-
-	return (d);
+    return (d);
 }
 
