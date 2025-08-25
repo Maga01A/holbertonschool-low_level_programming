30ee06f1e73a6fb064de6fa41587f595e6a98c0b
Maga01A <alnatluxanov@gmail.com>
2025-08-25 12:16:31 +0000

    Add struct dog definition

---
+++ structures_typedef/README.md
@@ -0,0 +1,9 @@
+# structures_typedef
+
+Bu proje Holberton School C programlama dersinin bir parçasıdır.
+Görev: struct dog adında bir yapı tanımlamak.
+
+Yapı şu bilgileri tutar:
+- name: köpeğin adı (char *)
+- age: köpeğin yaşı (float)
+- owner: sahibinin adı (char *)
---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - köpek yapısı
+ * @name: köpeğin adı
+ * @age: köpeğin yaşı
+ * @owner: sahibinin adı
+ *
+ * Açıklama: Bu struct bir köpeği temsil etmek için kullanılır.
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif
+
