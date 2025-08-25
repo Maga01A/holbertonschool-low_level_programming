097fe8a621c2874ec7b39c46022344f595fe850e
Maga01A <alnatluxanov@gmail.com>
2025-08-25 12:48:07 +0000

    5-free

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>  /* free için */
+#include "dog.h"     /* struct dog ve prototipler için */
+
+/**
+ * free_dog - Dinamik olarak oluşturulmuş bir dog_t'yi ve içindeki kopyaları serbest bırakır
+ * @d: Serbest bırakılacak dog_t işaretçisi
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)      /* Eğer dog_t işaretçisi NULL ise */
+		return;         /* hiçbir şey yapma */
+
+	if (d->name != NULL)  /* name alanı NULL değilse */
+		free(d->name);   /* name için ayrılmış hafızayı serbest bırak */
+
+	if (d->owner != NULL) /* owner alanı NULL değilse */
+		free(d->owner);  /* owner için ayrılmış hafızayı serbest bırak */
+
+	free(d);             /* dog_t için ayrılmış hafızayı serbest bırak */
+}
+
