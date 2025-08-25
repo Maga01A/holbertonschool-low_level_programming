551eb94249383e14255f6615e9b217ffe162d617
Maga01A <alnatluxanov@gmail.com>
2025-08-25 12:31:06 +0000

    3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -21,12 +21,9 @@ struct dog
 
 /* typedef ile struct dog için yeni isim dog_t oluşturuldu */
 typedef struct dog dog_t;
-
 /* init_dog fonksiyon prototipi */
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 /* print_dog fonksiyon prototipi */
 void print_dog(struct dog *d);
-
 #endif /* DOG_H */
 
