24a6d59f93c0a096f0270f7b2ce02895421ab7cf
Maga01A <alnatluxanov@gmail.com>
2025-08-26 14:48:55 +0000

    fixed code

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -28,13 +28,16 @@ void print_all(const char * const format, ...)
 			switch (format[i])
 			{
 				case 'c':
-					printf("%s%c", separator, va_arg(args, int));
+					printf("%s%c", separator, 
+						va_arg(args, int));
 					break;
 				case 'i':
-					printf("%s%d", separator, va_arg(args, int));
+					printf("%s%d", separator, 
+						va_arg(args, int));
 					break;
 				case 'f':
-					printf("%s%f", separator, va_arg(args, double));
+					printf("%s%f", separator, 
+						va_arg(args, double));
 					break;
 				case 's':
 					str = va_arg(args, char *);
