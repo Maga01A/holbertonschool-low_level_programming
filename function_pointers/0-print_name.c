b915b6e3c776506e8cafe64e65c2e0b3596221f5
Maga01A <alnatluxanov@gmail.com>
2025-08-26 13:19:03 +0000

    betty fixed

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include <stddef.h>            /* NULL üçün */
+#include <stddef.h>
 #include "function_pointers.h"
 
 /**
@@ -10,9 +10,9 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name == NULL || f == NULL)
-        return;
+	if (name == NULL || f == NULL)
+		return;
 
-    f(name);
+	f(name);
 }
 
