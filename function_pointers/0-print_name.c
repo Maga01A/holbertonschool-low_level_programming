6e2c696419f132b88ce1f24ee707fd067b0359a7
Maga01A <alnatluxanov@gmail.com>
2025-08-26 12:31:43 +0000

    Readme.md

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using a given function
+ * @name: name string to print
+ * @f: pointer to a function that takes a char* and returns nothing
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    if (name == NULL || f == NULL)
+        return;
+
+    f(name);
+}
+
---
+++ function_pointers/README.md
@@ -0,0 +1,12 @@
+# Function pointers
+
+This project is about learning how to use function pointers in C.
+
+## Files
+- 0-print_name.c: Function that prints a name using a function pointer.
+- function_pointers.h: Header file.
+
+## Compilation
+```bash
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 *.c -o a
+```
