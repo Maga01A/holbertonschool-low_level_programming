6d6efbe89ff9ee182f5d7ce50eb7cdd5cd902733
Maga01A <alnatluxanov@gmail.com>
2025-09-10 13:17:37 +0000

    All

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -23,7 +23,7 @@ int main(int ac, char *av[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
 	if (fd_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	rd_stat = 1;
 	while (rd_stat)
 	{
