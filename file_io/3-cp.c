8218eb4fe56ae69b8d494a9ad8f6db5017c79a81
Maga01A <alnatluxanov@gmail.com>
2025-09-10 14:59:09 +0000

    All

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -56,24 +56,33 @@ int main(int argc, char *argv[])
     {
         close(fd_from);
         error_exit_str(99, "Error: Can't write to %s\n", argv[2]);
-    }
+    }	
 
-    while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+    while (1)
     {
-        ssize_t total_written = 0;
+	    bytes_read = read(fd_from, buffer, BUFFER_SIZE);
+	    if (bytes_read == -1)
+	    {
+		    close(fd_from);
+		    close(fd_to);
+		    error_exit_str(98, "Error: Can't read from file %s\n", argv[1]);
+	    }
+	    if (bytes_read == 0) /* EOF */
+		    break;
 
-        while (total_written < bytes_read)
-        {
-            ssize_t bytes_written = write(fd_to, buffer + total_written,
-                                          bytes_read - total_written);
-            if (bytes_written == -1)
-            {
-                close(fd_from);
-                close(fd_to);
-                error_exit_str(99, "Error: Can't write to %s\n", argv[2]);
-            }
-            total_written += bytes_written;
-        }
+	    ssize_t total_written = 0;
+	    while (total_written < bytes_read)
+	    {
+		    ssize_t bytes_written = write(fd_to, buffer + total_written,
+				    bytes_read - total_written);
+		    if (bytes_written == -1)
+		    {
+			    close(fd_from);
+			    close(fd_to);
+			    error_exit_str(99, "Error: Can't write to %s\n", argv[2]);
+		    }
+		    total_written += bytes_written;
+	    }
     }
 
     if (bytes_read == -1)
